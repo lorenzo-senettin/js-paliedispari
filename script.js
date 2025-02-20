64c73df7346132c0c6ef0538dd1d758dd8210755
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-20 16:33:20 +0100

    add reverse word functiond

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const userWord = prompt(`Inserisci la parola da verificare:`);
 console.log(userWord);
 
 
+//reverse word
+function reverseWord(userWord) {
+  let word = ``;
+  for (let i = userWord.length - 1; i >= 0; i--) {
+    const thisChar = userWord[i];
+    word += thisChar;
+  }
+  return word;
+}
+
+const reversedWord = reverseWord(userWord);
+console.log(reversedWord);
