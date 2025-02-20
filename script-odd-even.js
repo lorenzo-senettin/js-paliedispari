2014ddbd1d91574f0b0f586884fd3e4221c0c0a3
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-02-20 16:44:23 +0100

    add odd-even script file and setup

---
+++ script-odd-even.js
@@ -0,0 +1,9 @@
+// value limit
+const limit = 5;
+
+// user number
+let userNumber;
+userNumber = Number(prompt("Inserisci un numero da 1 a 5:"));
+
+// pc number
+const pcNumber = Math.ceil(Math.random() * limit)
